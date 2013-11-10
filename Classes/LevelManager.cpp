6476b97f264ed9bfd42360f31813baca8a0c9c9f
jgnation <jeremy.gustine@gmail.com>
2013-11-10 14:36:25 -0600

    Levels have been divided into individual plist files.

--- Classes/LevelManager.cpp
+++ Classes/LevelManager.cpp
@@ -4,12 +4,15 @@ using namespace cocos2d;
 
 LevelManager::LevelManager()
 {
-	std::string levelsFileName = CCFileUtils::sharedFileUtils()->fullPathFromRelativePath("levels.plist");
-	_levels = CCDictionary::createWithContentsOfFileThreadSafe(levelsFileName.c_str());
-	_levels->retain();
+	//std::string levelsFileName = CCFileUtils::sharedFileUtils()->fullPathFromRelativePath("levels.plist");
+	//_levels = CCDictionary::createWithContentsOfFileThreadSafe(levelsFileName.c_str());
+	//_levels->retain();
 }
 
 Level * LevelManager::getLevel(int levelNumber)
 {
-	return new Level((CCDictionary *) _levels->objectForKey(std::to_string(levelNumber)));
+	//return new Level((CCDictionary *) _levels->objectForKey(std::to_string(levelNumber)));
+	std::string fileName = std::string("level_") + std::to_string(levelNumber) + ".plist";
+	std::string levelsFileName = CCFileUtils::sharedFileUtils()->fullPathFromRelativePath(fileName.c_str());
+	return new Level(CCDictionary::createWithContentsOfFileThreadSafe(levelsFileName.c_str()));
 }
