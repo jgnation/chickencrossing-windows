2c372f0ec4331d6e80467958aa888f82308514c7
jgnation <jeremy.gustine@gmail.com>
2013-11-18 22:57:26 -0600

    Added a title sprite and start-game-button to the main menu.  Updated TODO.

--- Classes/MenuButtonLayer.cpp
+++ Classes/MenuButtonLayer.cpp
@@ -10,16 +10,27 @@ bool MenuButtonLayer::init()
     {
         CC_BREAK_IF(! CCLayer::init());
 
+		//add title image
+		float originalWidth = 576;
+		float originalHeight = 144;
+		CCSprite * titleImage = CCSprite::create("chicken_crossing_title.png", CCRectMake(0, 0, originalWidth, originalHeight));
+		titleImage->setScale(.7);
+		titleImage->setPosition(ccp(CCDirector::sharedDirector()->getWinSize().width - 300, 350));
+		this->addChild(titleImage);
+
+		//add buttons
+
 		// Create a "close" menu item with close icon, it's an auto release object.
         CCMenuItemImage *pCloseItem = CCMenuItemImage::create(
-            "CloseNormal.png",
-            "CloseSelected.png",
+            "start_game_orange.png",
+            "start_game_yellow.png",
             this,
             menu_selector(MenuButtonLayer::menuStartGameCallback));
         CC_BREAK_IF(! pCloseItem);
 
+		pCloseItem->setScale(.5);
         // Place the menu item bottom-right conner.
-        pCloseItem->setPosition(ccp(CCDirector::sharedDirector()->getWinSize().width - 300, 300));
+        pCloseItem->setPosition(ccp(CCDirector::sharedDirector()->getWinSize().width - 300, 100));
 
         // Create a menu with the "close" menu item, it's an auto release object.
         CCMenu* pMenu = CCMenu::create(pCloseItem, NULL);
