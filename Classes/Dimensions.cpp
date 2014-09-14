5442d2eec07cbe59f846083b6d1b51f4cff3f029
jgnation <jeremy.gustine@gmail.com>
2014-09-14 14:27:53 -0500

    Chicken can no longer move out of the playing area at the top of the screen.

--- Classes/Dimensions.cpp
+++ Classes/Dimensions.cpp
@@ -25,7 +25,7 @@ bool Dimensions::moveIsInPlayableArea(CCPoint point, Chicken * chicken)
 {
 	//TODO: this method may belong in Chicken, not in Dimensions.
 	//initialize this only once
-	static CCRect * _playableArea = new CCRect(0, _laneWidth, (_windowSize.width - chicken->getSprite()->getContentSize().width), _windowSize.height - (_laneWidth * 3));
+	static CCRect * _playableArea = new CCRect(0, _laneWidth, (_windowSize.width - chicken->getSprite()->getContentSize().width), _windowSize.height - (_laneWidth * 4));
 	if (_playableArea->containsPoint(point))
 	{
 		return true;
