0c3ed721ed9b5eae4aa80937550d9037ddf9ce06
jgnation <jeremy.gustine@gmail.com>
2014-11-28 16:44:18 -0600

    These files should not be in source control.

--- proj.android/google-play-services_lib/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\cocos2d-x\cocos2d-x-3.2\cocos2d-x-3.2\projects\EggScramble\proj.android\google-play-services_lib\bin\AndroidManifest.xml : \
-C:\cocos2d-x\cocos2d-x-3.2\cocos2d-x-3.2\projects\EggScramble\proj.android\google-play-services_lib\AndroidManifest.xml \
